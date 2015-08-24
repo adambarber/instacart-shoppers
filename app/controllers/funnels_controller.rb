15618e3c1cdb46d1256727f677abb6d34754ac15
Adam Barber <adam@adambarber.tv>
2015-08-23 21:10:33 -0700

    add page titles

--- app/controllers/funnels_controller.rb
+++ app/controllers/funnels_controller.rb
@@ -2,7 +2,7 @@ class FunnelsController < ApplicationController
 
   def index
     start_date = params.fetch(:start_date, 1.week.ago)
-    end_date = params.fetch(:end_date, Time.now)
+    end_date = params.fetch(:end_date, Time.zone.now)
     @funnels = Funnel.applicants_by_stage(start_date, end_date)
     render json: @funnels.to_json
   end
